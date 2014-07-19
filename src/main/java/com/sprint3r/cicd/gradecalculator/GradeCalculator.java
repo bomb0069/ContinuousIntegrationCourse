b81495779b6ca019cbe0ac71d34a70b3219016c4
Karan Sivarat <bomb0069@gmail.com>
2014-07-19 05:27:46 -0400

    refector class to used linkedHashMap

--- src/main/java/com/sprint3r/cicd/gradecalculator/GradeCalculator.java
+++ src/main/java/com/sprint3r/cicd/gradecalculator/GradeCalculator.java
@@ -1,35 +1,29 @@
 package com.sprint3r.cicd.gradecalculator;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
 
 public class GradeCalculator {
 
+	static Map<Integer,String> scoreMap = new LinkedHashMap<Integer,String>();
+	static {
+		scoreMap.put(95, "A+");
+		scoreMap.put(85, "A");
+		scoreMap.put(80, "A-");
+		scoreMap.put(70, "B");
+		scoreMap.put(65, "C");
+		scoreMap.put(60, "D+");
+		scoreMap.put(55, "D");
+		scoreMap.put(50, "D-");
+		scoreMap.put(40, "F+");
+	}
+
 	public String cal(int score) {
+		for (Integer scoreLenght : scoreMap.keySet()) {
+			if(score >= scoreLenght)
+				return scoreMap.get(scoreLenght);
+		}
 		
-		if(score >= 95)
-			return "A+";
-		
-		if(score >= 85)
-			return "A";
-		
-		if(score >= 80)
-			return "A-";
-
-		if(score >= 70)
-			return "B";
-
-		if(score >= 65)
-			return "C";
-		
-		if(score >= 60)
-			return "D+";
-		
-		if(score >= 55)
-			return "D";
-
-		if(score >= 50)
-			return "D-";
-
-		if(score >= 40)
-			return "F+";
 		
 		return "F";
 	}
