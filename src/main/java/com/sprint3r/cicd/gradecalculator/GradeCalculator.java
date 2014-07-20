d4825a9e0ae858e8c4502c77af43cbab425c2910
tumit <tumit@live.com>
2014-07-20 11:53:52 +0700

    fixed tab char to space

--- src/main/java/com/sprint3r/cicd/gradecalculator/GradeCalculator.java
+++ src/main/java/com/sprint3r/cicd/gradecalculator/GradeCalculator.java
@@ -1,35 +1,35 @@
 package com.sprint3r.cicd.gradecalculator;
+
 import java.util.Map;
 import java.util.LinkedHashMap;
 
 public class GradeCalculator {
 
-	static Map<Integer,String> scoreMap = new LinkedHashMap<Integer,String>();
-	static {
-		scoreMap.put(95, "A+");
-		scoreMap.put(85, "A");
-		scoreMap.put(80, "A-");
-		scoreMap.put(70, "B");
-		scoreMap.put(65, "C");
-		scoreMap.put(60, "D+");
-		scoreMap.put(55, "D");
-		scoreMap.put(50, "D-");
-		scoreMap.put(40, "F+");
-	}
+    static Map<Integer, String> scoreMap = new LinkedHashMap<Integer, String>();
+    static {
+        scoreMap.put(95, "A+");
+        scoreMap.put(85, "A");
+        scoreMap.put(80, "A-");
+        scoreMap.put(70, "B");
+        scoreMap.put(65, "C");
+        scoreMap.put(60, "D+");
+        scoreMap.put(55, "D");
+        scoreMap.put(50, "D-");
+        scoreMap.put(40, "F+");
+    }
 
-	public String cal(int score) {
-		if(score > 100){
-			return "Invalid score";
-		}
-					
+    public String cal(int score) {
+        if (score > 100) {
+            return "Invalid score";
+        }
 
-		for (Integer scoreLenght : scoreMap.keySet()) {
-			if(score >= scoreLenght){
-				return scoreMap.get(scoreLenght);	
-			}
-		}
-		
-		return "F";
-	}
+        for (Integer scoreLenght : scoreMap.keySet()) {
+            if (score >= scoreLenght) {
+                return scoreMap.get(scoreLenght);
+            }
+        }
+
+        return "F";
+    }
 
 }
